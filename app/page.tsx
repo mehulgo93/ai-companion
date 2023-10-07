64d0bc04892a8319c050da9980731e1cab8f8057
Mehul Goel <111099204+mehulgo93@users.noreply.github.com>
2023-10-06 20:44:27 -0400

    Dark and Light Mode

--- app/page.tsx
+++
@@ -1,5 +0,0 @@
-import Image from "next/image";
-
-export default function Home() {
-  return <div className="text-red-500 text-3xl">hello world</div>;
-}
---
+++ components/Navbar.tsx
@@ -0,0 +1,42 @@
+"use client";
+import { cn } from "@/lib/utils";
+import { UserButton } from "@clerk/nextjs";
+import { Menu, Sparkle, Sparkles } from "lucide-react";
+import { Poppins } from "next/font/google";
+import Link from "next/link";
+import { Button } from "@/components/ui/button";
+import { ModeToggle } from "@/components/mode-toggle";
+
+const font = Poppins({
+  weight: "600",
+  subsets: ["latin"],
+});
+
+const Navbar = () => {
+  return (
+    <div className="fixed w-full z-50 flex justify-between items-center py-2 px-4 border-b border-primary/10 bg-secondary">
+      <div className="flex items-center">
+        <Menu className="block md:hidden" />
+        <Link href="/">
+          <h1
+            className={cn(
+              "hidden md:block text-xl md:text-3xl font-bold text-primary",
+              font.className
+            )}
+          >
+            Companion.ai
+          </h1>
+        </Link>
+      </div>
+      <div className="flex items-center gap-x-3">
+        <Button variant="premium" className="bg-white text-black" size="sm">
+          Upgrade <Sparkles className="h4 w-4 fill-black text-black ml-2" />
+        </Button>
+        <ModeToggle />
+        <UserButton />
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
