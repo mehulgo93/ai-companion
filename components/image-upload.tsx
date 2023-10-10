a5742f318596ebd8fa6caf948e0dba34f4a3dadb
Mehul Goel <111099204+mehulgo93@users.noreply.github.com>
2023-10-10 16:20:58 -0400

    adding desciption and name for your companion

--- components/image-upload.tsx
+++ components/image-upload.tsx
@@ -27,13 +27,17 @@ export const ImageUpload = ({
 
   return (
     <div className="space-y-4 w-full flex flex-col justify-center items-center">
-      <CldUploadButton options={{ maxFiles: 1 }} uploadPreset="o6sqfgze">
+      <CldUploadButton
+        onUpload={(result: any) => onChange(result.info.secure_url)}
+        options={{ maxFiles: 1 }}
+        uploadPreset="o6sqfgze"
+      >
         <div className="p-4 border-4 border-dashed border-primary/10 rounded-lg hover:opacity-75 transition flex flex-col space-y-2 items-center justify-center">
           <div className="relative h-40 w-40">
             <Image
               fill
               alt="upload"
-              src="/placeholder.svg"
+              src={value || "/placeholder.svg"}
               className="rounded-lg object-cover"
             />
           </div>
