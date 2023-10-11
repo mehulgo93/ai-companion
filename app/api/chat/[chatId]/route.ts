47fa5b545b8a10d1f85602612314bbca7c08a4e1
Mehul Goel <111099204+mehulgo93@users.noreply.github.com>
2023-10-11 18:41:08 -0400

    Working AI Companion

---
+++ app/api/chat/[chatId]/route.ts
@@ -0,0 +1,149 @@
+import dotenv from "dotenv";
+import { StreamingTextResponse, LangChainStream } from "ai";
+import { auth, currentUser } from "@clerk/nextjs";
+import { Replicate } from "langchain/llms/replicate";
+import { CallbackManager } from "langchain/callbacks";
+import { NextResponse } from "next/server";
+
+import { MemoryManager } from "@/lib/memory";
+import { rateLimit } from "@/lib/rate-limit";
+import prismadb from "@/lib/prismadb";
+
+dotenv.config({ path: `.env` });
+
+export async function POST(
+  request: Request,
+  { params }: { params: { chatId: string } }
+) {
+  try {
+    const { prompt } = await request.json();
+    const user = await currentUser();
+
+    if (!user || !user.firstName || !user.id) {
+      return new NextResponse("Unauthorized", { status: 401 });
+    }
+
+    const identifier = request.url + "-" + user.id;
+    const { success } = await rateLimit(identifier);
+
+    if (!success) {
+      return new NextResponse("Rate limit exceeded", { status: 429 });
+    }
+
+    const companion = await prismadb.companion.update({
+      where: {
+        id: params.chatId
+      },
+      data: {
+        messages: {
+          create: {
+            content: prompt,
+            role: "user",
+            userId: user.id,
+          },
+        },
+      }
+    });
+
+    if (!companion) {
+      return new NextResponse("Companion not found", { status: 404 });
+    }
+
+    const name = companion.id;
+    const companion_file_name = name + ".txt";
+
+    const companionKey = {
+      companionName: name!,
+      userId: user.id,
+      modelName: "llama2-13b",
+    };
+    const memoryManager = await MemoryManager.getInstance();
+
+    const records = await memoryManager.readLatestHistory(companionKey);
+    if (records.length === 0) {
+      await memoryManager.seedChatHistory(companion.seed, "\n\n", companionKey);
+    }
+    await memoryManager.writeToHistory("User: " + prompt + "\n", companionKey);
+
+    // Query Pinecone
+
+    const recentChatHistory = await memoryManager.readLatestHistory(companionKey);
+
+    // Right now the preamble is included in the similarity search, but that
+    // shouldn't be an issue
+
+    const similarDocs = await memoryManager.vectorSearch(
+      recentChatHistory,
+      companion_file_name
+    );
+
+    let relevantHistory = "";
+    if (!!similarDocs && similarDocs.length !== 0) {
+      relevantHistory = similarDocs.map((doc) => doc.pageContent).join("\n");
+    }
+    const { handlers } = LangChainStream();
+    // Call Replicate for inference
+    const model = new Replicate({
+      model:
+        "meta/llama-2-70b-chat:02e509c789964a7ea8736978a43525956ef40397be9033abf9fd2badfe68c9e3",
+      input: {
+        max_length: 2048,
+      },
+      apiKey: process.env.REPLICATE_API_TOKEN,
+      callbackManager: CallbackManager.fromHandlers(handlers),
+    });
+
+    // Turn verbose on for debugging
+    model.verbose = true;
+
+    const resp = String(
+      await model
+        .call(
+          `
+        ONLY generate plain sentences without prefix of who is speaking. DO NOT use ${companion.name}: prefix. 
+
+        ${companion.instructions}
+
+        Below are relevant details about ${companion.name}'s past and the conversation you are in.
+        ${relevantHistory}
+
+
+        ${recentChatHistory}\n${companion.name}:`
+        )
+        .catch(console.error)
+    );
+
+    const cleaned = resp.replaceAll(",", "");
+    const chunks = cleaned.split("\n");
+    const response = chunks[0];
+
+    await memoryManager.writeToHistory("" + response.trim(), companionKey);
+    var Readable = require("stream").Readable;
+
+    let s = new Readable();
+    s.push(response);
+    s.push(null);
+    if (response !== undefined && response.length > 1) {
+      memoryManager.writeToHistory("" + response.trim(), companionKey);
+
+      await prismadb.companion.update({
+        where: {
+          id: params.chatId
+        },
+        data: {
+          messages: {
+            create: {
+              content: response.trim(),
+              role: "system",
+              userId: user.id,
+            },
+          },
+        }
+      });
+    }
+
+    return new StreamingTextResponse(s);
+  } catch (error) {
+    return new NextResponse("Internal Error", { status: 500 });
+  }
+};
